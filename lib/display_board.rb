2362f16f82091c4af52743b44c0f050c7b717e55
Uzochukwu Obeki <uobeki.40@gmail.com>
2019-09-07 19:55:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  arr = [" ","|"," ","|"," "]
-  arr2 = [" ","|"," ","|"," "]
-  arr3 = [" ","|"," ","|"," "]
+       
+       
   
-  print arr
-  print "-----------"
-  print arr2
-  print "-----------"
-  print arr3
